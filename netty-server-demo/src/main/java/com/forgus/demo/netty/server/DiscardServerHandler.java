f14cd46a87264931deb126e0472a30b31f742548
孤峰 <chenwenbin@cai-inc.com>
2020-09-22 20:33:03 +0800

    ..

---
+++ netty-server-demo/src/main/java/com/forgus/demo/netty/server/DiscardServerHandler.java
@@ -0,0 +1,24 @@
+package com.forgus.demo.netty.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        try {
+            // Do something with msg
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
